a1b3719898f4b505fa3dba7d2dea61ee698a89a9
Robert Fletcher <Robert@Roberts-iMac.local>
2013-12-15 15:37:55 +0000

    Added Email Funtionality using Mandrill api

---
+++ email.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'src/Mandrill.php';
+require_once 'settings';
+
+function sendEmail($to,$from,$subject,$mssg){
+	global $mandrillApiKey;
+	global $mandrillTemplate;
+	$mandrill = new Mandrill($mandrillApiKey);
+	$message = array(
+	    'subject' => $subject,
+	    'from_email' => $from,
+	    'html' => $message,
+	    'to' => array(array('email' => $to)),
+	    'merge_vars' => array(array(
+	        'rcpt' => $to,
+	        'vars' =>
+	        array(
+				array(
+	                'name' => 'MESSAGE',
+	                'content' => $mssg )
+	    ))));
+	
+	$template_name = $mandrillTemplate;
+	
+	$template_content;
+	$mandrill->messages->sendTemplate($template_name, $template_content, $message);
+	
+}
+
+
+?>
--- settings.php
+++ settings.php
@@ -1,6 +1,12 @@
 <?php
+	//Data Base Settings
 	$db_user_name = "yoyoambi_root";
 	$db_password ="hackathon"; 	// If this is your password for the love of god change it
 	$db_host = "localhost"; 		// if unsure leave it alone
 	$db_db = "yoyoambi_yoyo";
-?>
+	
+	
+	// Mandrill Settings
+	$mandrillApiKey = 'P1L2-9STVK_J5P4HVbzVTg';
+	$mandrillTemplate  = 'testsend'
+;?>
