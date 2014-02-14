0bef3eb89ce9e576fd56121dc59328af803c3186
robrotheram <robrotheram@gmail.com>
2014-02-14 15:19:41 +0000

    Update settings.php
    Removed private things from

--- ambitionengine/settings.php
+++ ambitionengine/settings.php
@@ -1,9 +1,9 @@
 <?php
 	//Data Base Settings
-	$db_user_name = "yoyoambi_root";
-	$db_password ="hackathon"; 	// If this is your password for the love of god change it
-	$db_host = "localhost"; 		// if unsure leave it alone
-	$db_db = "yoyoambi_yoyo";
+	$db_user_name = "";
+	$db_password =""; 	// If this is your password for the love of god change it
+	$db_host = ""; 		// if unsure leave it alone
+	$db_db = "";
 	
 	
 	// Mandrill Settings
